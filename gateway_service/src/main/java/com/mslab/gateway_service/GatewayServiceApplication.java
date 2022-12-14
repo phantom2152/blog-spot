f5fe21df669e13631d055f47f1b9c901df3291c4
Anish Gowda <anishgowda43@gmail.com>
2022-12-14 19:53:00 +0530

    corrected gateway

--- gateway_service/src/main/java/com/mslab/gateway_service/GatewayServiceApplication.java
+++ gateway_service/src/main/java/com/mslab/gateway_service/GatewayServiceApplication.java
@@ -2,10 +2,7 @@ package com.mslab.gateway_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
-
 @SpringBootApplication
-@EnableEurekaClient
 public class GatewayServiceApplication {
 
 	public static void main(String[] args) {
